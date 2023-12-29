1ae2ee56fe0e9399990bf5a48dd9f9ce545cd758
Yonghun <yonghuni484@gmail.com>
2023-12-29 23:21:19 +0900

    Feat: 로그인 메인 페이지 추가

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import SigninForm from "./_component/SigninForm";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Let's - 로그인",
@@ -7,10 +7,36 @@ export const metadata: Metadata = {
 
 const Signin = () => {
   return (
-    <section>
-      <h1>로그인 페이지</h1>
-      <SigninForm />
-    </section>
+    <div className="w-full">
+      <div className="flex flex-col justify-center items-center mt-[180px]">
+        <img
+          src="mainTitle.svg"
+          alt="메인 타이틀"
+          width="150px"
+          className="mb-4"
+        />
+        <p>내 손안의 최고 여행 선택!</p>
+      </div>
+      <div className="flex flex-col justify-center items-center m-20">
+        <Link
+          href="/signin"
+          className="w-[327px] h-[56px] rounded-lg bg-[#1FBB64] flex justify-center items-center text-white mb-4"
+        >
+          <img src="./icons/naverLogo.svg" alt="네이버 로고" className="mr-4" />
+          <div>네이버 로그인</div>
+        </Link>
+        <Link
+          href="/signin"
+          className="w-[327px] h-[56px] rounded-lg bg-yellow flex justify-center items-center mb-8 text-brown"
+        >
+          <img src="./icons/kakaoLogo.svg" alt="카카오 로고" className="mr-4" />
+          <div>카카오 로그인</div>
+        </Link>
+        <Link href="/signin" className="text-black-4">
+          이메일로 시작하기 &gt;
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,11 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    fontFamily: {
+      sans: ["Pretendard", "sans-serif"],
+      serif: ["Pretendard", "serif"],
+    },
+
     extend: {
       colors: {
         black: "#000",
