da0e4a680953d188ff2fb1a5411a696af27cc427
Yonghun <yonghuni484@gmail.com>
2023-12-29 21:43:02 +0900

    Design: 로그인 페이지 공통 css 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,35 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#000",
+        "black-1": "#111",
+        "black-2": "#222",
+        "black-3": "#333",
+        "black-4": "#444",
+        "black-6": "#666",
+        "grey-1": "#F4F4F6",
+        "grey-2": "#ddd",
+        "grey-3": "#ccc",
+        "grey-4": "#888",
+        "blue-transparent": "rgba(1, 82, 207, 0.50)",
+        red: "#EE1439",
+        blue: "#0059E3",
+        orange: "#E67100",
+        white: "#fff",
+        brown: "#3D1200",
+        green: "#03C75B",
+        pink: "#FF3D73",
+        yellow: "#FFD600",
+      },
+      borderRadius: {
+        lg: "12px",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
