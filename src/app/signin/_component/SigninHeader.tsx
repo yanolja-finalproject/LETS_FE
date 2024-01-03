6ef20f2a43e0891f539261679da897b7ef738c05
Yonghun <yonghuni484@gmail.com>
2024-01-03 21:58:17 +0900

    Feat: 헤더 테마 추가

--- src/app/signin/_component/SigninHeader.tsx
+++ src/app/signin/_component/SigninHeader.tsx
@@ -3,28 +3,40 @@ import Link from "next/link";
 interface Props {
   text: string;
   subText?: string;
-  redirectUrl: string;
+  redirectUrl?: string;
+  theme?: string;
 }
 
-const SigninHeader = ({ text, subText, redirectUrl }: Props) => {
+const SigninHeader = ({
+  text,
+  subText = "",
+  redirectUrl,
+  theme = "default",
+}: Props) => {
   return (
     <div className="flex relative h-[48px]">
-      <Link
-        href={redirectUrl}
-        className="absolute left-[32px] top-1/2 -translate-y-1/2"
-      >
-        <img src="./icons/leftArrowIcon.svg" alt="왼쪽 화살표" width="24px" />
-      </Link>
+      {redirectUrl && (
+        <Link
+          href={redirectUrl}
+          className="absolute left-[32px] top-1/2 -translate-y-1/2"
+        >
+          <img src="./icons/leftArrowIcon.svg" alt="왼쪽 화살표" width="24px" />
+        </Link>
+      )}
+
       <div className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 text-[18px] text-black-2 font-semibold">
-        <span>{text}</span>
-        <span className="font-thin">{subText}</span>
+        {theme === "default" && (
+          <>
+            <span>{text}</span>
+            <span className="font-thin">{subText}</span>
+          </>
+        )}
+        {theme === "main" && (
+          <img src="./icons/mainTitle.svg" alt="메인 로고" width={75} />
+        )}
       </div>
     </div>
   );
-};
-
-SigninHeader.defaultProps = {
-  subText: "",
 };
 
 export default SigninHeader;
