7b9b66ce440941269ee6336bce8d4375eb6e1079
sunpaaaa <sunpaaaa@gmail.com>
2024-01-04 19:04:30 +0900

    Feat: 검색 필터 페이지 이동 박스 생성

---
+++ src/app/search/_component/ForwardBox.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+
+const ForwardBox = () => {
+  return (
+    <div className="flex flex-col justify-evenly items-center h-56 bg-blue-1">
+      <p className="text-black-2 font-semibold text-[18px]">패키지 상품 찾기</p>
+      <span className="text-grey-4 text-center">
+        국가, 테마별, 가격 등 필터를 이용해서 <br />
+        나에게 맞는 패키지상품을 빠르게 찾아보세요.
+      </span>
+      <Link
+        href={"/search-hashtag"}
+        className="flex justify-evenly items-center rounded-full bg-gradient-to-br from-pink-1 to-pink-2 w-[54px] h-[54px]"
+      >
+        <img
+          src="./icons/optionsIcon.svg"
+          alt="해시태그로 검색하기 페이지 바로가기"
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default ForwardBox;
