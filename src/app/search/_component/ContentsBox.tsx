ca94e46fcd9fbdd830de2b7b16ba97402a15566f
sunpaaaa <sunpaaaa@gmail.com>
2024-01-05 00:58:21 +0900

    Feat: 검색 컨텐츠 박스 컴포넌트 생성

---
+++ src/app/search/_component/ContentsBox.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ContentsBox = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <section className="ml-6">
+      <p className="text-black-2 font-semibold text-[18px]">{title}</p>
+      {children}
+    </section>
+  );
+};
+
+export default ContentsBox;
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ForwardBox from "./_component/ForwardBox";
+import ContentsBox from "./_component/ContentsBox";
 
 const SearchKeywordPage = () => {
   return (
     <div className="flex flex-col w-full">
-      <div>여행 추천 태그</div>
-      <div>많이 본 여행지</div>
+      <ContentsBox title="여행 추천 태그">
+        <div>hashtages</div>
+      </ContentsBox>
+      <ContentsBox title="최근 많이 본 여행지">
+        <div>card items</div>
+      </ContentsBox>
       <ForwardBox />
     </div>
   );
