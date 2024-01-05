ddb213ba4e167a236b7107d6f0a72baee929c03b
developer-jyyun <developer.jyyun@gmail.com>
2024-01-05 19:15:35 +0900

    Chore: 메인페이지 하단 내비게이션 원복

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import BottomNav from "./_component/common/BottomNav";
+
 const Home = async () => {
   return (
     <section>
       <h1>메인 페이지</h1>
+      <BottomNav />
     </section>
   );
 };
