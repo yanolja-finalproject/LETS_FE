5e392c5aa34d353c58fe7142268ca9d69412f09e
developer-jyyun <developer.jyyun@gmail.com>
2024-01-08 04:01:56 +0900

    Feat: 메뉴 페이지, 탈퇴 페이지 UI

---
+++ src/app/my/menu/_component/AuthItems.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { ListItemProps } from "../../_types/my.types";
+import List from "./List";
+import ConfirmModal from "./ConfirmModal";
+import Withdraw from "./Withdraw";
+
+// TODO:: 비회원 사용자에게는 노출 X
+const AuthItems = () => {
+  const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isWithdrawing, setIsWithdrawing] = useState(false);
+
+  const handleLogout = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Logout");
+    router.push("/");
+  };
+
+  const confirmWithdraw = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsModalOpen(true);
+  };
+
+  const handleWithdraw = () => {
+    setIsWithdrawing(true);
+    setTimeout(() => {
+      console.log("탈퇴😂");
+      setIsWithdrawing(false);
+      router.push("/");
+    }, 3000);
+  };
+  const AuthMenu: ListItemProps[] = [
+    {
+      title: "로그아웃",
+      theme: "menu",
+      onClickFn: handleLogout,
+    },
+    {
+      title: "회원 탈퇴",
+      theme: "menu",
+      onClickFn: confirmWithdraw,
+    },
+  ];
+
+  return (
+    <>
+      <List items={AuthMenu} />
+      {!isWithdrawing && <List items={AuthMenu} />}
+      <ConfirmModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onConfirm={() => {
+          setIsModalOpen(false);
+          handleWithdraw();
+        }}
+      />
+      {isWithdrawing && <Withdraw />}
+    </>
+  );
+};
+
+export default AuthItems;
