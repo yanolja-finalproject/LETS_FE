0c46107e7c5469fb80debdaf43f1c99f6b3fa3e5
sunpaaaa <sunpaaaa@gmail.com>
2024-01-07 16:14:39 +0900

    Feat: 하단 네비바 유무에 따른 레이아웃 분리

---
+++ src/app/(navbar)/layout.tsx
@@ -0,0 +1,12 @@
+import BottomNav from "../_component/common/BottomNav";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <main>{children}</main>
+      <BottomNav />
+    </>
+  );
+};
+
+export default Layout;
