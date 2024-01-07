22b0adebc394626a843f1d45421a3437891f1187
developer-jyyun <developer.jyyun@gmail.com>
2024-01-08 08:19:57 +0900

    Design: 탈퇴 모달 web사이즈 지정

--- src/app/my/menu/_component/ConfirmModal.tsx
+++ src/app/my/menu/_component/ConfirmModal.tsx
@@ -10,10 +10,8 @@ const ConfirmModal = ({ isOpen, onClose, onConfirm }: Props) => {
   return (
     // TODO: 모달 컴포넌트 분리 예정
     <div className="fixed top-0 left-0 w-full h-full bg-black-6">
-      <div className="w-[87.2%] mx-auto absolute top-[30%] left-[50%] translate-x-[-50%] rounded-md bg-white h-[176px] flex flex-col justify-evenly ">
-        <p className="text-4.5 font-bold text-center">
-          정말로 탈퇴 하시겠어요?
-        </p>
+      <div className="w-[87.2%] web:w-[327px] mx-auto absolute top-[30%] left-[50%] translate-x-[-50%] rounded-md bg-white h-[176px] flex flex-col justify-evenly ">
+        <p className="text-lg font-bold text-center">정말로 탈퇴 하시겠어요?</p>
         <div className="flex flex-row justify-between items-between px-[72px]">
           <button
             className="text-black-4 font-medium"
