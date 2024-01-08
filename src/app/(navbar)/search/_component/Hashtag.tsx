718a88090da36c2acee14e4f6409123ad4c74302
sunpaaaa <sunpaaaa@gmail.com>
2024-01-08 10:54:47 +0900

    Style: 인라인 스타일에서 테일윈드로 변경

--- src/app/(navbar)/search/_component/Hashtag.tsx
+++ src/app/(navbar)/search/_component/Hashtag.tsx
@@ -11,8 +11,7 @@ const Hashtag = ({ data }: Props) => {
 
   return (
     <div
-      className="text-black-4 px-3 py-1 flex justify-center items-center border border-solid border-grey-2 rounded-full active:bg-pink-2 active:text-white active:border-pink-2"
-      style={{ fontSize, fontWeight }}
+      className={`text-black-4 px-3 py-1 flex justify-center items-center border border-solid border-grey-2 rounded-full active:bg-pink-2 active:text-white active:border-pink-2 ${fontSize} ${fontWeight}`}
     >
       {data.name}
     </div>
--- src/utils/hashtagStyle.ts
+++ src/utils/hashtagStyle.ts
@@ -1,6 +1,6 @@
 const hashtagStyle = (rank: number) => {
-  const fontSize = `${10 + (10 - rank)}px`;
-  const fontWeight = rank < 3 ? "bold" : "normal";
+  const fontSize = `text-[${10 + (10 - rank)}px]`;
+  const fontWeight = rank < 3 ? "font-bold" : "font-normal";
 
   return { fontSize, fontWeight };
 };
