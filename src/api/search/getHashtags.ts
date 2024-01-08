8ae5a713a635032862d3153e9e8f14db239b6284
sunpaaaa <sunpaaaa@gmail.com>
2024-01-08 10:58:49 +0900

    Fix: vercel 배포에러 해결

--- src/api/search/getHashtags.ts
+++ src/api/search/getHashtags.ts
@@ -1,5 +1,9 @@
 const getHashtags = async () => {
   const hashtags = await fetch("http://localhost:8123/v1/search/hashtags");
+
+  if (!hashtags.ok) {
+    throw new Error("Failed to fetch data");
+  }
   return hashtags.json();
 };
 
