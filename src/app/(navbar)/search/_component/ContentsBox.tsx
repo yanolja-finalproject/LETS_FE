b3a082a43ef43797a98fe6b97da35f712907ddd8
sunpaaaa <sunpaaaa@gmail.com>
2024-01-09 11:49:36 +0900

    Refactor: props interface 분리

--- src/app/(navbar)/search/_component/ContentsBox.tsx
+++ src/app/(navbar)/search/_component/ContentsBox.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-const ContentsBox = ({
-  title,
-  children,
-}: {
+interface Props {
   title: string;
   children: React.ReactNode;
-}) => {
+}
+
+const ContentsBox = ({ title, children }: Props) => {
   return (
     <section className="px-6">
       <p className="my-6 text-black-2 font-semibold text-[18px] whitespace-pre-line">
