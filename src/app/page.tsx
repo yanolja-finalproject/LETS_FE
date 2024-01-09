1adfb7bb7bf56d5c22fe047a0d2fd380786307ca
applevalley <skatkd7571@naver.com>
2024-01-09 11:44:34 +0900

    Refactor: 메인페이지 레이아웃 margin 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,20 @@ import HomeThemePackage from "./(navbar)/home/_component/HomeThemePackage";
 
 const Home = async () => {
   return (
-    <section className="flex flex-col items-center [&>*:nth-child(n+2)]:mb-10">
+    <section>
       <DefaultHeader text="" theme="main" />
-      {/* 광고구좌 */}
-      <HomeAdvertisements />
-      {/* 해시태그 검색 */}
-      <HomeHashtags />
-      {/* 패키지 컨셉 */}
-      <HomeThemePackage />
-      {/* 찬반토론 */}
-      <HomeProsAndCons />
-      {/* 초특가 패키지 목록 */}
-      <HomePackages />
+      <div className="flex flex-col items-center [&>*:nth-child(n)]:mb-10">
+        {/* 광고구좌 */}
+        <HomeAdvertisements />
+        {/* 해시태그 검색 */}
+        <HomeHashtags />
+        {/* 패키지 컨셉 */}
+        <HomeThemePackage />
+        {/* 찬반토론 */}
+        <HomeProsAndCons />
+        {/* 초특가 패키지 목록 */}
+        <HomePackages />
+      </div>
       <BottomNav />
     </section>
   );
